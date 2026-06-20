89463de1a5f5bd860cdd27a6f6abeaaca3af2a38
arcade_kappa <gkappa@outlands.top>
2026-06-20 22:12:00 +0800

    Update CRL template

---
+++ src/main/java-templates/gkappa/modernsplash/Reference.java
@@ -0,0 +1,13 @@
+package {{ package }};
+
+/**
+ * Tags storage class, you can change at will
+ */
+public class Reference {
+    private Reference() {}
+
+    public static final String MOD_ID = "{{ mod_id }}";
+    public static final String MOD_NAME = "{{ mod_name }}";
+    public static final String VERSION = "{{ mod_version }}";
+
+}
